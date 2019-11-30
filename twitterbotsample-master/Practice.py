f817d245995cd16f595fbe4b808f94c126cf3f4a
Shreyas Myana <31059819+shreyasmyana@users.noreply.github.com>
2019-11-30 15:46:16 +0530

    Update Practice.py

--- twitterbotsample-master/Practice.py
+++ twitterbotsample-master/Practice.py
@@ -1,7 +1,7 @@
-CONSUMER_KEY = 'T6juvyiQgcB4fIt3WUV5tjUq2'
-CONSUMER_SECRET = 'NQDzlwjF7bQYk5MOdeZp6NsdMRAehL916CsP87iqyAC92OZd24'
-ACCESS_KEY = '2346369812-iiNNtF5qarI641KnkI31bF0hdQ2XAO5VdcMHcgV'
-ACCESS_SECRET = 'hpG2njU6t8beWPJujLrFrQqUzdIaiTPe5DU8KlL6Ltss9'
+CONSUMER_KEY = '**********'
+CONSUMER_SECRET = '***********'
+ACCESS_KEY = '*************'
+ACCESS_SECRET = '*********'
 import tweepy
 print('This is my TwitterBot')
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
